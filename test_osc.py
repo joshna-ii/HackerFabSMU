c364b6afcf11f3c086421fecbdbccfed2853cbcf
= <jiyengar@andrew.cmu.edu>
2024-03-21 22:26:49 -0400

    working for ideal mosfet

--- test_osc.py
+++ test_osc.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt   # needed for plotting
  
 """-----------------------------------------------------------------------"""
  
-#resistance = float(input("Enter the resistance in Ohms of the resistor in series with the mosfet: "))
-resistance = 5100
+#resistance = float(input("Enter the resistance in Ohms of the resistor in series with the mosfet: ")) TODO user inputs
+resistance = 5
 # connect to the device
-device_data = device.open()
+device_data = device.open() #TODO open again to get second device
  
 """-----------------------------------"""
  
@@ -15,33 +15,15 @@ device_data = device.open()
 scope.open(device_data, sampling_frequency=10e5)
 
 # generate a 10KHz sine signal with 2V amplitude on channel 1
-wavegen.generate(device_data, channel=1, function=wavegen.function.sine, offset=1, frequency=10e2, amplitude=2)
-'''
-mosfet_voltages = scope.record(device_data, channel=1)
-wavegen.close(device_data)
-
-wavegen.generate(device_data, channel=1, function=wavegen.function.sine, offset=1, frequency=10e2, amplitude=2)
-resistor_voltages = scope.record(device_data, channel=2)
-wavegen.close(device_data)
-'''
-[mosfet_voltages, resistor_voltages] = scope.record2(device_data)
-wavegen.close(device_data)
-
-mosfet_currents = []
-for v in resistor_voltages:
-    mosfet_currents.append(v/resistance)
-
-added_voltages = []
-for i in range(len(resistor_voltages)):
-    added_voltages.append(mosfet_voltages[i] + resistor_voltages[i])
-print("mosfet voltages")
-print(mosfet_voltages)
-print("resistor voltages")
-print(resistor_voltages)
-print("added voltages")
-print(added_voltages)
-print("mosfet currents")
-print(mosfet_currents)
+for i in range(0.8, 5, 0.2):
+    wavegen.generate(device_data, channel=2, function=wavegen.function.dc, offset=i, frequency=10e2, amplitude=1) #generate dc signal to gate voltage at voltage i
+    for j in range(0, 5, 0.1):
+        wavegen.generate(device_data, channel=1, function=wavegen.function.dc, offset=j, frequency=10e2, amplitude=2.5)
+        [resistor_voltages, mosfet_voltages] = scope.record2(device_data) #functions outputs [resistor_voltages, mosfet_voltages]
+        mosfet_currents = []
+        for v in resistor_voltages:
+            mosfet_currents.append(v/resistance)
+        plt.plot(mosfet_voltages, mosfet_currents)
 
 n = len(mosfet_voltages)
 
@@ -50,31 +32,23 @@ for t in range(0,n):
     time.append(0.4*t/n)      
 
 #plt.plot(time, mosfet_voltages)
+#plt.plot(time, mosfet_voltages5)
 #plt.plot(time, resistor_voltages)
 #plt.plot(time, mosfet_currents)
-plt.plot(mosfet_voltages, mosfet_currents)
 #plt.plot(resistor_voltages, mosfet_currents)
-#plt.xlabel("Voltage [V]")
-#plt.ylabel("Current [A]")
+#plt.plot(resistor_voltages5, mosfet_currents5)
+#plt.plot(mosfet_voltages, mosfet_currents)
+#plt.plot(mosfet_voltages5, mosfet_currents5)
+plt.xlabel("Voltage (V_DS) [V]")
+plt.ylabel("Current (I_D) [A]")
 plt.show()
-print("here")
 
 
-'''
-# plot
-time = [moment * 1e03 for moment in time]   # convert time to ms
-plt.plot(time, buffer)
-plt.xlabel("time [ms]")
-plt.ylabel("voltage [V]")
-plt.show()
-'''
 # reset the scope
 scope.close(device_data)
  
 # reset the wavegen
-#wavegen.close(device_data)
- 
-"""-----------------------------------"""
+wavegen.close(device_data)
  
 # close the connection
 device.close(device_data)
