a60d039959859498d9abd5a30c65925b409b083d
= <jiyengar@andrew.cmu.edu>
2024-04-02 10:37:50 -0400

    added some user inputs

--- smu.py
+++ smu.py
@@ -6,36 +6,40 @@ import csv #needed for generating CSV files for graphing later
  
 """-----------------------------------------------------------------------"""
  
-#resistance = float(input("Enter the resistance in Ohms of the resistor in series with the mosfet: "))
-resistance = 100
-gate_voltages = [0, 1, 2, 3, 4, 5] #gate voltages to sweep across
+drain_resistance = float(input("Enter the resistance in Ohms of the resistor in series with the mosfet DRAIN: "))
+gate_resistance = float(input("Enter the resistance in Ohms of the resistor in series with the mosfet GATE: "))
+chip_number = input("Enter your chip number (ex. 305): ")
+device_id = input("Enter the device being tested (ex. 2b1): ")
+gate_voltages = input("Enter the gate voltages to test as a comma-separated list (Ex. 1, 1.5, 1.6, 3): ").replace(" ", "").split(",")
 # name of csv files
-filename_currents = "chip_currents.csv"
-filename_voltages = "chip_voltages.csv"
+filename_currents = f"csvfiles/{chip_number}_{device_id}_currents.csv"
+filename_voltages = f"csvfiles/{chip_number}_{device_id}_voltages.csv"
 # connect to the device
-device_data = device.open() #TODO open again to get second device
+ad3_data1 = device.open() #TODO open again to get second device
  
 """-----------------------------------"""
 
 # writing to csv file  
 for filename in [filename_currents, filename_voltages]:
-    with open(f'csvfiles/{filename}', 'w') as csvfile: # opens csv files
+    with open(filename, 'w') as csvfile: # opens csv files
         csvwriter = csv.writer(csvfile)  # creating a csv writer object 
         csvwriter.writerow(gate_voltages) # writes header row (gate voltages)
  
 # initialize the scope with default settings
-scope.open(device_data, sampling_frequency=10e5)
+scope.open(ad3_data1, sampling_frequency=10e5)
+
+wavegen.generate(ad3_data1, channel=1, function=wavegen.function.sine, offset=5, frequency=10e2, amplitude=5) #generation sine waveform to drain
 
 # generate a 10KHz sine signal with 2V amplitude on channel 1
 current_dict = {}
 volt_dict = {}
 for VG in gate_voltages:
-    wavegen.generate(device_data, channel=2, function=wavegen.function.dc, offset=VG, frequency=10e2, amplitude=1) #generate dc signal to gate voltage at voltage i
-    wavegen.generate(device_data, channel=1, function=wavegen.function.sine, offset=2.5, frequency=10e2, amplitude=2.5) #generation sine waveform to drain
-    [mosfet_voltages, resistor_voltages] = scope.record2(device_data) # get data with AD3 oscilloscope
+    VG = float(VG)
+    wavegen.generate(ad3_data1, channel=2, function=wavegen.function.dc, offset=VG, frequency=10e2, amplitude=1) #generate dc signal to gate voltage at voltage i
+    [resistor_voltages, mosfet_voltages] = scope.record2(ad3_data1) # get data with AD3 oscilloscope
     mosfet_currents = []
     for v in resistor_voltages:
-        mosfet_currents.append(v/resistance) # calculate current with ohms law
+        mosfet_currents.append(v/drain_resistance) # calculate current with ohms law
     for filename in [filename_currents, filename_voltages]: #outputs currents and voltages to csv
         with open(filename,'a') as csvfile:
             writer = csv.writer(csvfile)
@@ -51,10 +55,10 @@ plt.ylabel("Current (I_D) [A]")
 plt.show()
 
 # reset the scope
-scope.close(device_data)
+scope.close(ad3_data1)
  
 # reset the wavegen
-wavegen.close(device_data)
+wavegen.close(ad3_data1)
  
 # close the connection
-device.close(device_data)
+device.close(ad3_data1)
